121d1fe1586756462730e405c60e909573388270
Theuzyn2330 <matheusrex2330@gmail.com>
2026-08-31 14:07:03 -0300

    chore: add initial models structure

---
+++ src/Models/AbstractModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+abstract class AbstractModel
+{
+    public function __construct(array $attributes = [])
+    {
+        foreach ($attributes as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+}
---
+++ src/Models/Source.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Models;
+
+class Source extends AbstractModel
+{
+}
