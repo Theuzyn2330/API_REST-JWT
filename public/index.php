761e45c0fc33fb1bc62cd39b36c6612d5a4d0a27
Theuzyn2330 <matheusrex2330@gmail.com>
2026-09-19 11:40:05 -0300

    feat: setup front controller entry point

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+header('Content-Type: application/json; charset=UTF-8');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
